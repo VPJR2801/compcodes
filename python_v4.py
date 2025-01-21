e6df12b9eea3980847d44d010ee9d7112484411a
VPJR2801 <vishwa1928@gmail.com>
2025-01-21 11:58:19 +0530

    Add files via upload

---
+++ python_v4.py
@@ -0,0 +1,6 @@
+def remove_substring(string1,string2):
+    return string1.replace(string2, '')
+string1=input("Enter parent string:")
+string2=input("Enter the string to remove:")
+finalstring=remove-substring(string1,string2)
+print(finalstring)
